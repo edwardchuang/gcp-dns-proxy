c291dc6993c2a11a4555e12610e1735e9d0ae48c
Edward Chuang <edwardc@pixnet.tw>
2016-11-07 10:58:58 +0800

    add ip for primary and secondary ns

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ global.config = {
   'key_dir': '/etc/path/to/service_account_json/',
   'default_ttl': 30,
   'primary_ns': 'ns1.blah.foo.com',
+  'primary_ip': '',
+  'secondary_ns': [''],
+  'secondary_ip': [''],
   'soa_admin':	'edwardchuang.gmail.com',
   'domains': ['']
 }
